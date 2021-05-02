2ae5683c4ea75a222761623daa51c3b99cc1cdf5
Divya vishwakarma <66290338+Divya527@users.noreply.github.com>
2021-05-02 19:01:43 +0530

    DigitalClock.java

---
+++ DigitalClock.java
@@ -0,0 +1,25 @@
+
+import java.awt.FlowLayout; 
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DigitalClock extends JFrame{
+    
+    JLabel jlabClock;
+    
+    public DigitalClock(){
+       jlabClock = new JLabel("");
+       setLayout(new FlowLayout());
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       add(jlabClock);
+       setSize(1200,120);
+       setLocationRelativeTo(null);
+       setVisible(true);
+    }
+    
+    public static void main(String arr[]){
+        new DigitalClock();
+    }
+}
